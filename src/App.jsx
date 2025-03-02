a84ce7da3901f308f2bdbc9d076f2b728503df63
Yogalakshmi <yogarajakanibe@gmail.com>
2025-03-02 07:03:24 +0530

    react router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router-dom";
 
 const routes = [
   {
-    path:'/',
-    element:<h1>Hello world</h1>
-  }
+    path: "/",
+    element: <h1>Hello world</h1>,
+  },
 ];
-const router=createBrowserRouter(routes,{
-future:{
-  v7_relativeSplatPath:true,
-  v7_fetcherPersist:true,
-  v7_normalizeFormMethod:true,
-  v7_partialHydration:true,
-  v7_skipActionErrorRevalidation:true,
-}
-})
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
 const App = () => {
-  return <RouterProvider
-  router={router}
-  future={{
-    v7_startTransition: true,
-  }}/>
-}
+  return (
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  );
+};
 
-export default App
+export default App;
